f107ea93caab7cc1e1d5c7e1d6cbcb8cb0c2eb1c
Ourcodingadventure <Javitan0309@gmail.com>
2022-01-12 21:35:55 +0200

    .

--- maps-backend/config/env.js
+++ maps-backend/config/env.js
@@ -3,5 +3,4 @@ let env = {
     "mongodb+srv://mapTeam:theteam1@cluster0.tb9kc.mongodb.net/MapApp?retryWrites=true&w=majority",
   SERVER_SECRET: process.env.SERVER_SECRET || "1234",
 };
-
 module.exports = env;
